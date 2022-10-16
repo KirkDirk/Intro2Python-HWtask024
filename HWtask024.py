a5371b48fe65b13ab220ecf8a0590772d9549329
Kirk Dirk <jackalex@ya.ru>
2022-10-16 15:26:54 +0500

    исправлена декомпрессия с дву- и более значными числами + lambda

--- HWtask024.py
+++ HWtask024.py
@@ -2,6 +2,7 @@
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 ## формируем сжатую строку и записываем в файл 2
+
 data1 = open('C:\Sas\LearningGB\Intro2Python\HW\HWtask024\data1.txt', 'r')
 data2 = open('C:\Sas\LearningGB\Intro2Python\HW\HWtask024\data2.txt', 'w')
 string1 = data1.read()
@@ -24,12 +25,22 @@ data2.close()
 ## формируем расжатую строку и записываем в файл 3
 with open('C:\Sas\LearningGB\Intro2Python\HW\HWtask024\data2.txt', 'r') as data2:
     string2 = data2.read()
+list1=[]
+list2=[]
 string3 = ''
-for char in string2:
-    if char.isdigit(): 
-        temp = char
+temp = string2[0]
+list1.append(temp)
+i=1
+while i < len(string2):
+    if not string2[i].isdigit():
+        list2.append(string2[i])
+    elif string2[i].isdigit() and string2[i-1].isdigit():
+        temp += string2[i]
+        list1[index_temp] = temp 
     else:
-        string3 += char * int(temp)
-        temp = ''
-with open('C:\Sas\LearningGB\Intro2Python\HW\HWtask024\data3.txt', 'w') as data3:
-    data3.write(string3)
+        temp = string2[i]
+        list1.append(string2[i])
+        index_temp = len(list1)-1
+    i += 1
+list3 = list(map(lambda x, y: int(x)*y, list1, list2))
+string3 = ''.join(list3)
